25c0c6e6809931692989f7bd2c37f9379d7adb07
GiovaniRibetech <giovani.ribeiro@ribetech.com.br>
2024-09-09 10:43:10 +0000

    rendimento

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,43 +2,43 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AlertController, IonicModule } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: true,
-  imports: [IonicModule,FormsModule,AlertController]
+  imports: [IonicModule, FormsModule],
+  standalone: true
+
 })
 export class HomePage {
   public gasolina: number | undefined;
   public alcool: number | undefined;
   public razao: number | undefined;
   public situacao: string = "";
-
   constructor(private alertController: AlertController) { }
 
-    calcularBene(){
-      if(this.gasolina && this.alcool){
-        this.razao = this.alcool / this.gasolina;
-        if(this.razao <= 0.7){
-          this.situacao = "Álcool compensa";
-        }
-        if(this.razao > 0.7){
-          this.situacao = "Gasolina compensa";
-        }
-        else{
-          this.exibirAlerta();
-        }
+  calcularBene() {
+    if (this.gasolina && this.alcool) {
+      this.razao = this.alcool / this.gasolina;
+      if (this.razao <= 0.7) {
+        this.situacao = "Álcool compensa";
+      }
+      else if (this.razao > 0.7) {
+        this.situacao = "Gasolina compensa";
+      }
+      else {
+        this.exibirAlerta();
       }
     }
+  }
 
-    async exibirAlerta() {
-      const alert = await this.alertController.create({
+  async exibirAlerta() {
+    const alert = await this.alertController.create({
       header: 'Atenção',
-      message: 'Por favor, digite as duas notas antes de calcular a média',
+      message: 'Por favor, digite os doas valores antes de fazer o cálculo',
       buttons: ['OK']
-      });
-
-      await alert.present();
-}
+    });
+    await alert.present();
+  }
 }
