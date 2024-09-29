a0c80ad4925994b8cf2254e6e23148cbf6f40031
Giovani Ribeiro <giovani.ribeiro@ribetech.com.br>
2024-09-29 22:59:35 +0000

    funciona

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { AlertController, IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  public notaA: number | undefined;
+  public notaB: number | undefined;
+  public media: number | undefined;
+  public situacao: string = "";
+
+  constructor(private alertController: AlertController) {}
+
+  calcularMedia(){
+
+    if(this.notaA && this.notaB){
+      this.media = (this.notaA + this.notaB) / 2;
+      this.situacao = this.media >= 7 ? 'Aprovado' : 'Reprovado';
+    }
+    else {
+      this.exibirAlerta();
+    }
+  }
+
+  async exibirAlerta(){
+    const alert = await this.alertController.create({
+      header: 'Atenção',
+      message: 'Por favor, digite as duas notas antes de calcular a média',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
